a4dc3047deab749d63a9ecb998816ab01917696a
akrhode <a.rhode@web.de>
2023-04-26 00:12:40 +0200

    Connect json data to display card content

--- src/App.js
+++ src/App.js
@@ -3,18 +3,31 @@ import TourCard from "./components/TourCard";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import SearchAppBar from "./components/Appbar";
+import cities from "./data.json";
+import Typography from "@mui/material/Typography";
 
 function App() {
   return (
     <div className="App">
       <SearchAppBar />
       <Container sx={{ marginY: 5 }}>
-        <Grid container spacing={5}>
-          <TourCard />
-          <TourCard />
-          <TourCard />
-          <TourCard />
-        </Grid>
+        {cities.map((city) => (
+          <>
+            <Typography
+              variant="h4"
+              component="h2"
+              marginTop={5}
+              marginBottom={3}
+            >
+              Top {city.name} Tours
+            </Typography>
+            <Grid container spacing={5}>
+              {city.tours.map((tour, index) => (
+                <TourCard tour={tour} key={index} />
+              ))}
+            </Grid>
+          </>
+        ))}
       </Container>
     </div>
   );
--- src/components/TourCard.js
+++ src/components/TourCard.js
@@ -1,10 +1,14 @@
-import Paper from "@mui/material/Paper";
-import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
-import Typography from "@mui/material/Typography";
 import { AccessTime } from "@mui/icons-material";
-import Rating from "@mui/material/Rating";
-import { createTheme, ThemeProvider } from "@mui/material";
+import {
+  createTheme,
+  Grid,
+  Paper,
+  Rating,
+  ThemeProvider,
+  Typography,
+} from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
 
 const theme = createTheme({
   components: {
@@ -31,43 +35,59 @@ const theme = createTheme({
   },
 });
 
-const TourCard = () => {
+export default function Card({ tour }) {
   return (
-    <Grid item xs={3}>
+    <Grid item xs={4} md={3}>
       <ThemeProvider theme={theme}>
-        <Paper elevation={3}>
-          <img
-            src="https://images.pexels.com/photos/2929906/pexels-photo-2929906.jpeg?auto=compress&cs=tinysrgb&w=600"
-            alt="Macchu Picchu"
-            className="img"
-          />
-          <Box paddingX={1}>
+        <Paper elevation={3} className="paper">
+          <img src={tour.image} alt="" className="img" />
+          <Box
+            sx={{
+              paddingX: 1,
+            }}
+          >
             <Typography variant="subtitle1" component="h2">
-              Visit the Incas
+              {tour.name}
             </Typography>
-            <Box sx={{ display: "flex", alignItems: "center" }} marginTop={3}>
-              <AccessTime sx={{ width: 12.5 }} />
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              <AccessTime style={{ width: 12.5 }} />
               <Typography variant="body2" component="p" marginLeft={0.5}>
-                7 hours
+                {tour.duration} hours
               </Typography>
             </Box>
-            <Box sx={{ display: "flex", alignItems: "center" }} marginTop={3}>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+              }}
+              marginTop={3}
+            >
               <Rating
-                name="read-only"
-                value={4.5}
-                precision={0.5}
+                name="size-small"
                 size="small"
+                defaultValue={tour.rating}
+                precision={0.25}
+                readOnly
               />
               <Typography variant="body2" component="p" marginLeft={0.5}>
-                4.5
+                {tour.rating}
               </Typography>
               <Typography variant="body3" component="p" marginLeft={1.5}>
-                (500 reviews)
+                ({tour.numberOfReviews} reviews)
               </Typography>
             </Box>
-            <Box>
-              <Typography variant="h6" component="h3" marginTop={0}>
-                From $150
+            <Box
+              sx={{
+                display: "flex",
+              }}
+            >
+              <Typography variant="h6" component="h2" marginTop={0}>
+                From EUR {tour.price}
               </Typography>
             </Box>
           </Box>
@@ -75,6 +95,4 @@ const TourCard = () => {
       </ThemeProvider>
     </Grid>
   );
-};
-
-export default TourCard;
+}
