ace6e80fe0d5e89cb4ec0a3bebacc1c8e78ad7f9
akrhode <a.rhode@web.de>
2023-04-25 21:57:33 +0200

    Create card content

--- src/components/TourCard.js
+++ src/components/TourCard.js
@@ -1,10 +1,45 @@
 import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import { AccessTime } from "@mui/icons-material";
+import Rating from "@mui/material/Rating";
 
 const TourCard = () => {
   return (
     <Grid item xs={3}>
-      <Paper elevation={3}>Hello</Paper>
+      <Paper elevation={3}>
+        <img
+          src="https://images.pexels.com/photos/2929906/pexels-photo-2929906.jpeg?auto=compress&cs=tinysrgb&w=600"
+          alt="Macchu Picchu"
+          className="img"
+        />
+        <Box paddingX={1}>
+          <Typography variant="subtitle1" component="h2">
+            Visit the Incas
+          </Typography>
+          <Box sx={{ display: "flex", alignItems: "center" }} marginTop={3}>
+            <AccessTime sx={{ width: 12.5 }} />
+            <Typography variant="body2" component="p" marginLeft={0.5}>
+              7 hours
+            </Typography>
+          </Box>
+          <Box sx={{ display: "flex", alignItems: "center" }} marginTop={3}>
+            <Rating name="read-only" value={4.5} precision={0.5} size="small" />
+            <Typography variant="body2" component="p" marginLeft={0.5}>
+              4.5
+            </Typography>
+            <Typography variant="body3" component="p" marginLeft={1.5}>
+              (500 reviews)
+            </Typography>
+          </Box>
+          <Box>
+            <Typography variant="h6" component="h3" marginTop={0}>
+              From $150
+            </Typography>
+          </Box>
+        </Box>
+      </Paper>
     </Grid>
   );
 };
