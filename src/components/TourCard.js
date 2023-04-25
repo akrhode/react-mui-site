a62437ec7aa5159e5ab0cd9dc5eebb3e62b17100
akrhode <a.rhode@web.de>
2023-04-25 22:06:41 +0200

    Customize typography with theme provider

--- src/components/TourCard.js
+++ src/components/TourCard.js
@@ -4,42 +4,75 @@ import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import { AccessTime } from "@mui/icons-material";
 import Rating from "@mui/material/Rating";
+import { createTheme, ThemeProvider } from "@mui/material";
+
+const theme = createTheme({
+  components: {
+    MuiTypography: {
+      variants: [
+        {
+          props: {
+            variant: "body2",
+          },
+          style: {
+            fontSize: 11,
+          },
+        },
+        {
+          props: {
+            variant: "body3",
+          },
+          style: {
+            fontSize: 9,
+          },
+        },
+      ],
+    },
+  },
+});
 
 const TourCard = () => {
   return (
     <Grid item xs={3}>
-      <Paper elevation={3}>
-        <img
-          src="https://images.pexels.com/photos/2929906/pexels-photo-2929906.jpeg?auto=compress&cs=tinysrgb&w=600"
-          alt="Macchu Picchu"
-          className="img"
-        />
-        <Box paddingX={1}>
-          <Typography variant="subtitle1" component="h2">
-            Visit the Incas
-          </Typography>
-          <Box sx={{ display: "flex", alignItems: "center" }} marginTop={3}>
-            <AccessTime sx={{ width: 12.5 }} />
-            <Typography variant="body2" component="p" marginLeft={0.5}>
-              7 hours
+      <ThemeProvider theme={theme}>
+        <Paper elevation={3}>
+          <img
+            src="https://images.pexels.com/photos/2929906/pexels-photo-2929906.jpeg?auto=compress&cs=tinysrgb&w=600"
+            alt="Macchu Picchu"
+            className="img"
+          />
+          <Box paddingX={1}>
+            <Typography variant="subtitle1" component="h2">
+              Visit the Incas
             </Typography>
+            <Box sx={{ display: "flex", alignItems: "center" }} marginTop={3}>
+              <AccessTime sx={{ width: 12.5 }} />
+              <Typography variant="body2" component="p" marginLeft={0.5}>
+                7 hours
+              </Typography>
+            </Box>
+            <Box sx={{ display: "flex", alignItems: "center" }} marginTop={3}>
+              <Rating
+                name="read-only"
+                value={4.5}
+                precision={0.5}
+                size="small"
+              />
+              <Typography variant="body2" component="p" marginLeft={0.5}>
+                4.5
+              </Typography>
+              <Typography variant="body3" component="p" marginLeft={1.5}>
+                (500 reviews)
+              </Typography>
+            </Box>
+            <Box>
+              <Typography variant="h6" component="h3" marginTop={0}>
+                From $150
+              </Typography>
+            </Box>
           </Box>
-          <Box sx={{ display: "flex", alignItems: "center" }} marginTop={3}>
-            <Rating name="read-only" value={4.5} precision={0.5} size="small" />
-            <Typography variant="body2" component="p" marginLeft={0.5}>
-              4.5
-            </Typography>
-            <Typography variant="body3" component="p" marginLeft={1.5}>
-              (500 reviews)
-            </Typography>
-          </Box>
-          <Box>
-            <Typography variant="h6" component="h3" marginTop={0}>
-              From $150
-            </Typography>
-          </Box>
-        </Box>
-      </Paper>
+        </Paper>
+      </ThemeProvider>
     </Grid>
   );
 };
